6f68b905ef805f2e96fbe8a0330db179eecfe865
Sachin C Kasar <cse.sck@gmail.com>
2018-01-18 20:12:56 +0530

    updated
    updated

--- src/app/components/TwoWheeler.js
+++ src/app/components/TwoWheeler.js
@@ -3,9 +3,7 @@ import {render} from "react-dom";
 // TwoWheel as a Class Component
 export const TwoWheeler = (props) =>  {
 	  return ( 
-		    <div className="container">
-                <div className="row">
-				    <div className="row">
+		    <div  className="alert alert-warning" >
 						<h1 style={{color:'blue'}}> Two Wheeler </h1>
 						<ul>
 							<li>
@@ -25,11 +23,10 @@ export const TwoWheeler = (props) =>  {
 							 
 						</ul>
 						<div className="btn-group btn-group-sm">
-							<button onClick={() => props.changeVisitor('New Visitor')} className="btn btn-primary" >Update Visitor</button>
-							<button onClick={() => props.changeDiscount('10%')} className="btn btn-primary" >Update Discount</button>
+							<button style={{margin:'5px'}} onClick={() => props.changeVisitor('New Visitor')} className="btn btn-primary" >Update Visitor</button>
+							<button style={{margin:'5px'}}  onClick={() => props.changeDiscount('10%')} className="btn btn-primary" >Update Discount</button>
 						</div>
-					</div>
-				</div>
+					
 				</div>
       
         );
